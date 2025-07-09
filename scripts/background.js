a8c5be621e11064698d9c2ae86ed69d7dcf47db3
natheraf <ma.eric11214@outlook.com>
2025-07-08 20:52:14 -0400

    chore: catch repeating error

--- scripts/background.js
+++ scripts/background.js
@@ -2,16 +2,17 @@ self.importScripts("./tabs.js");
 
 chrome?.tabs?.onUpdated?.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.url?.startsWith("https://leetcode.com/problems/")) {
-    chrome.tabs.sendMessage(tabId, {
-      message: "problem on screen",
-      url: changeInfo.url,
-    });
+    chrome.tabs
+      .sendMessage(tabId, {
+        message: "problem on screen",
+        url: changeInfo.url,
+      })
+      .catch(console.error);
   }
 });
 
 chrome?.runtime?.onMessage?.addListener((request, sender) => {
   if (request.message === "close me") {
-    console.log(request);
     chrome.tabs.remove(request.tabId);
   }
 });
