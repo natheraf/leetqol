c8da7b76308c4ddc8be3b1a8622f22b137d07d98
natheraf <ma.eric11214@outlook.com>
2025-07-18 16:30:57 -0400

    chore: removed unused selectors

--- scripts/selectors.config.js
+++ scripts/selectors.config.js
@@ -46,15 +46,6 @@ const dPathSelectors = {
     "path[d='M496 200c0 13.3-10.7 24-24 24h0H360 328c-13.3 0-24-10.7-24-24s10.7-24 24-24h32 54.1l-52.1-52.1C333.8 95.8 295.7 80 256 80c-72.7 0-135.2 44.1-162 107.1c-5.2 12.2-19.3 17.9-31.5 12.7s-17.9-19.3-12.7-31.5C83.9 88.2 163.4 32 256 32c52.5 0 102.8 20.8 139.9 57.9L448 142.1V88l0-.4V56c0-13.3 10.7-24 24-24s24 10.7 24 24V200zM40 288H152c13.3 0 24 10.7 24 24s-10.7 24-24 24H97.9l52.1 52.1C178.2 416.2 216.3 432 256 432c72.6 0 135-43.9 161.9-106.8c5.2-12.2 19.3-17.8 31.5-12.6s17.8 19.3 12.6 31.5C427.8 424 348.5 480 256 480c-52.5 0-102.8-20.8-139.9-57.9L64 369.9V424c0 13.3-10.7 24-24 24s-24-10.7-24-24V312c0-13.3 10.7-24 24-24z']",
 };
 
-const submissionTabSelectors = {
-  submissionsListDiv:
-    "html > body > div:nth-of-type(1) > div:nth-of-type(2) > div > div > div:nth-of-type(4) > div > div > div:nth-of-type(7) > div > div:nth-of-type(2)",
-};
-
-const clockResetSelector =
-  "html > body > div:nth-of-type(1) > div:nth-of-type(2) > div > div > div:nth-of-type(3) > nav > div:nth-of-type(3) > div:nth-of-type(2) > div:nth-of-type(4) > div > div > div:nth-of-type(1) > div > div:nth-of-type(3)";
-const codeResetSelector =
-  "html > body > div:nth-of-type(1) > div:nth-of-type(2) > div > div > div:nth-of-type(4) > div > div > div:nth-of-type(8) > div > div:nth-of-type(1) > div:nth-of-type(2) > button:nth-of-type(4)";
 const difficultyChipClasses = {
   easy: "text-difficulty-easy",
   medium: "text-difficulty-medium",
